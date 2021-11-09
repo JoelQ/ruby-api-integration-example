c3c0a8dc22d56d51ef7a3e30537f7d87bab7789f
Joël Quenneville <joelq@thoughtbot.com>
2021-11-09 16:32:12 -0500

    Introduce retries
    The client now attempts to get a new token if it gets a 401 response and
    then retries the original request.

--- lib/consumer/client.rb
+++ lib/consumer/client.rb
@@ -6,8 +6,19 @@ module Consumer
   class Client
     DOMAIN = "http://localhost:4567"
 
+    def initialize(client_id:, client_secret:)
+      @client_id = client_id
+      @client_secret = client_secret
+    end
+
     def fetch_movies
-      response = HTTParty.get(DOMAIN + "/movies")
+      response = HTTParty.get("#{DOMAIN}/movies?token=#{@token}")
+
+      if response.code == 401
+        refresh_token
+        response = HTTParty.get("#{DOMAIN}/movies?token=#{@token}")
+      end
+
       json = JSON.parse(response.body)
       json.map do |record|
         Movie.new(
@@ -16,6 +27,17 @@ module Consumer
           gross: record.fetch("gross"),
         )
       end
+    end
+
+    private
+
+    def refresh_token
+      response = HTTParty.post(
+        "#{DOMAIN}/new_token",
+        body: {client_id: @client_id, client_secret: @client_secret}.to_json
+      )
+
+      @token = response.body
     end
   end
 end
--- spec/lib/consumer/client_spec.rb
+++ spec/lib/consumer/client_spec.rb
@@ -3,13 +3,37 @@ require "consumer/client"
 RSpec.describe Consumer::Client do
   describe "#fetch_movies" do
     it "fetches movies from the API" do
-      stub_request(:get, "#{Consumer::Client::DOMAIN}/movies").
+      stub_request(:get, "#{Consumer::Client::DOMAIN}/movies?token=").
         to_return(body: [
           {name: "Avatar", director: "James Cameron", gross: 12345},
           {name: "Titanic", director: "James Cameron", gross: 78910}
         ].to_json)
 
-      client = Consumer::Client.new
+      client = Consumer::Client.new(client_id: "id", client_secret: "secret")
+      movies = client.fetch_movies
+
+      avatar = movies.first
+      expect(avatar.name).to eq "Avatar"
+      expect(avatar.gross).to eq 12345
+      expect(avatar.director).to eq "James Cameron"
+      expect(movies.length).to eq 2
+    end
+
+    it "refreshes token and retries request when given a 401" do
+      stub_request(:get, "#{Consumer::Client::DOMAIN}/movies?token=").
+        to_return(status: 401)
+
+      stub_request(:post, "#{Consumer::Client::DOMAIN}/new_token")
+        .with(body: {client_id: "id", client_secret: "secret"}.to_json)
+        .to_return(body: "good")
+
+      stub_request(:get, "#{Consumer::Client::DOMAIN}/movies?token=good").
+        to_return(body: [
+          {name: "Avatar", director: "James Cameron", gross: 12345},
+          {name: "Titanic", director: "James Cameron", gross: 78910}
+        ].to_json)
+
+      client = Consumer::Client.new(client_id: "id", client_secret: "secret")
       movies = client.fetch_movies
 
       avatar = movies.first
